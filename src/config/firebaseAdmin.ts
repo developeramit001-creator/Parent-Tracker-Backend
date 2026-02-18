ddc2e1c0fa7ff6fb357ba05a6d95a07e51a896d1
Amit <amitkumar1723p@gmail.com>
2026-02-18 08:01:10 +0530

    before integrate map section

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,16 +1,49 @@
+// import admin from "firebase-admin";
+
+// const serviceAccount = JSON.parse(
+//     process.env.FIREBASE_SERVICE_ACCOUNT as string
+// );
+
+// serviceAccount.private_key =
+//     serviceAccount.private_key.replace(/\\n/g, "\n");
+
+// if (!admin.apps.length) {
+//     admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount),
+//     });
+// }
+
+// export default admin;
+
+
+
+
+
+
+
 import admin from "firebase-admin";
+import path from "path";
 
-const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT as string
+const serviceAccountPath = path.join(
+    process.cwd(),
+    "firebase-admin.json"
 );
-
-serviceAccount.private_key =
-    serviceAccount.private_key.replace(/\\n/g, "\n");
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(serviceAccountPath),
     });
 }
 
 export default admin;
+
+
+
+
+
+
+
+
+
+
+
