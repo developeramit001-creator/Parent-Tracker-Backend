a45b64afd96adfdf4c3854b42cf9d632d31759b4
Amit <amitkumar1723p@gmail.com>
2026-03-07 08:25:29 +0530

    fix firebase admin file for production

--- dist/config/firebaseAdmin.js
+++ dist/config/firebaseAdmin.js
@@ -1,26 +1,18 @@
 "use strict";
-// import admin from "firebase-admin";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const serviceAccount = JSON.parse(
-//     process.env.FIREBASE_SERVICE_ACCOUNT as string
-// );
-// serviceAccount.private_key =
-//     serviceAccount.private_key.replace(/\\n/g, "\n");
-// if (!admin.apps.length) {
-//     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount),
-//     });
-// }
-// export default admin;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const path_1 = __importDefault(require("path"));
-const serviceAccountPath = path_1.default.join(process.cwd(), "firebase-admin.json");
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key =
+    serviceAccount.private_key.replace(/\\n/g, "\n");
 if (!firebase_admin_1.default.apps.length) {
     firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(serviceAccountPath),
+        credential: firebase_admin_1.default.credential.cert(serviceAccount),
     });
 }
 exports.default = firebase_admin_1.default;
--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,49 +1,18 @@
-// import admin from "firebase-admin";
-
-// const serviceAccount = JSON.parse(
-//     process.env.FIREBASE_SERVICE_ACCOUNT as string
-// );
-
-// serviceAccount.private_key =
-//     serviceAccount.private_key.replace(/\\n/g, "\n");
-
-// if (!admin.apps.length) {
-//     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount),
-//     });
-// }
-
-// export default admin;
-
-
-
-
-
-
-
 import admin from "firebase-admin";
-import path from "path";
 
-const serviceAccountPath = path.join(
-    process.cwd(),
-    "firebase-admin.json"
-);
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+serviceAccount.private_key =
+    serviceAccount.private_key.replace(/\\n/g, "\n");
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountPath),
+        credential: admin.credential.cert(serviceAccount),
     });
 }
 
 export default admin;
-
-
-
-
-
-
-
-
-
-
-
